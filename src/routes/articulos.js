a84b8309d1dfaf637c7b87f12c0a24f3db3574b4
unknown <andresfelipehc@ufps.edu.co>
2022-03-26 15:50:42 -0500

    Agragadas funcionalidades CRUD de articulos

--- src/routes/articulos.js
+++ src/routes/articulos.js
@@ -4,14 +4,83 @@ const pool = require('../database');
 
 
 router.post('/addArticulo', async(req, res)=>{
-    
+
+    const  {
+            id_articulo,
+            nombre_revista,
+            titulo_articulo,
+            autores_articulo,
+            año_articulo,
+            mes_articulo,
+            volumen_articulo,
+            pagina_inical,
+            pagina_final,
+            issn_articulo,
+            url_articulo,
+            doi_articulo        
+            } = req.body;
+
+    newArticulo ={
+            id_articulo,
+            nombre_revista,
+            titulo_articulo,
+            autores_articulo,
+            año_articulo,
+            mes_articulo,
+            volumen_articulo,
+            pagina_inical,
+            pagina_final,
+            issn_articulo,
+            url_articulo,
+            doi_articulo  
+    }
+    await pool.query('INSERT INTO articulo SET ?', [newArticulo]);
+    res.redirect('articulos');
 });
 
 router.get('/articulos', async (req, res)=>{
 
-    const libros = await pool.query('SELECT * FROM articulo');
+    const articulos = await pool.query('SELECT * FROM articulo');
+    res.json({articulos});
+});
 
-    res.json({libros});
+router.post('/editArt/:id_articulo', async(req, res)=>{
+    const { id_articulo} = req.params;
+    const {
+            nombre_revista,
+            titulo_articulo,
+            autores_articulo,
+            año_articulo,
+            mes_articulo,
+            volumen_articulo,
+            pagina_inical,
+            pagina_final,
+            issn_articulo,
+            url_articulo,
+            doi_articulo    
+    } = req.body;
+
+    await pool.query('UPDATE articulo SET nombre_revista =?, titulo_articulo =?, autores_articulo =?, año_articulo =?, mes_articulo =?, volumen_articulo =?, pagina_inical =?, pagina_final =?, issn_articulo =?, url_articulo =?, doi_articulo =? WHERE id_articulo =?', 
+    [   nombre_revista,
+        titulo_articulo,
+        autores_articulo,
+        año_articulo,
+        mes_articulo,
+        volumen_articulo,
+        pagina_inical,
+        pagina_final,
+        issn_articulo,
+        url_articulo,
+        doi_articulo,
+        id_articulo]);
+
+        res.redirect('/art/articulos'); 
+});
+
+router.get('/deleteArt/:id_articulo', async(req, res)=>{
+    const { id_articulo } = req.params;
+    await pool.query('DELETE FROM articulo WHERE id_articulo= ?', [id_articulo]);
+    res.redirect('/art/articulos'); 
 });
 
 module.exports = router;
--- src/routes/libros.js
+++ src/routes/libros.js
@@ -42,7 +42,7 @@ router.get('/libros', async (req, res)=>{
 });
 
 router.post('/editLibro/:id_libro', async(req, res)=>{
-    console.log('Entre');
+    
     const { id_libro } = req.params;
     const { 
         titulo_libro, 
@@ -55,8 +55,6 @@ router.post('/editLibro/:id_libro', async(req, res)=>{
         certificado_investigacion_libro,
         numero_capitulos_libro } = req.body;
 
-        console.log(id_libro);
-
     await pool.query('UPDATE libro SET titulo_libro = ?, isbn_libro = ?, fecha_publicacion_libro = ?, autores_libro = ?, editorial_libro = ?, lugar_publicacion_libro = ?, certificado_creditos_libro = ?, certificado_investigacion_libro = ?, numero_capitulos_libro =? WHERE id_libro = ?', 
         [titulo_libro, 
         isbn_libro, 
@@ -68,8 +66,6 @@ router.post('/editLibro/:id_libro', async(req, res)=>{
         certificado_investigacion_libro,
         numero_capitulos_libro, 
         id_libro]);
-
-        console.log('Ya guarde');
 
         res.redirect('/lib/libros');    
 });
