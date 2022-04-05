a7e08dcb615a21ba45751b9ee9648976881e5404
unknown <andresfelipehc@ufps.edu.co>
2022-04-05 16:04:28 -0500

    Se agregaron nuevos parametros para producto, se corrigio el error de insert y se agrego mensaje de eror para buscar producto por id

--- src/routes/producto.js
+++ src/routes/producto.js
@@ -3,25 +3,38 @@ const router = express.Router();
 const pool = require('../database');
 
 
-
 router.get('/productos', async (req, res)=>{
-    const productos = await pool.query('SELECT * FROM producto');
-    res.json(productos);
+
+    try{
+        const productos = await pool.query('SELECT * FROM producto');
+        res.json(productos);
+    }catch(e){
+        res.json(e);
+    }
 });
 
 router.post('/addProd', async (req, res)=>{
     const {
-        id,
         nombre,
-        cantidad
+        cantidad,
+        subcategoria,
+        fecha,
+        grupo,
+        programa,
+        facultad
     } = req.body;
 
-    const newProd ={
-        id,
+    const newProd ={      
         nombre,
-        cantidad
+        cantidad,
+        subcategoria,
+        fecha,
+        grupo,
+        programa,
+        facultad
     }
-    await pool.query('INSERT INTO set ?', [newProd]);
+    console.log(newProd);
+    await pool.query('INSERT INTO producto SET ?', [newProd]);
     res.redirect('productos');
 });
 
@@ -35,17 +48,29 @@ router.post('/editProd/:id', async(req, res)=>{
     const {id} = req.params;
     const {
         nombre,
-        cantidad
+        cantidad,
+        subcategoria,
+        fecha,
+        grupo,
+        programa,
+        facultad
     } = req.body;
 
-    await pool.query('UPDATE producto SET nombre = ?, cantidad = ? WHERE id_producto = ?', [nombre, cantidad, id]);
+    await pool.query('UPDATE producto SET nombre = ?, cantidad = ?, subcategoria = ?, fecha = ?, grupo = ?, programa = ?, facultad =?  WHERE id = ?', [nombre, cantidad, subcategoria, fecha, grupo, programa, facultad, id]);
     res.redirect('/prod/productos');
 });
 
 router.get('/findById/:id', async(req, res)=>{
     const {id} = req.params;
     const producto = await pool.query('SELECT * FROM producto WHERE id = ?', [id]);
-    res.json(producto);
+
+    if(producto == null){
+        res.json(producto);
+    }else{
+        msg = 'El usuario no existe'
+        res.json(msg);
+    }
+    
 });
 
 
