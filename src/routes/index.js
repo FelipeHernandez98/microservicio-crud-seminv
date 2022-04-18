75b0c6a2eed6c756a8c52f6a6195497cca415fb4
unknown <andresfelipehc@ufps.edu.co>
2022-04-18 13:32:40 -0500

    Se agrega el CRUD para subcategoria de los productos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const libro = require('./libros');
 const articulo = require('./articulos');
 const producto = require('./producto');
+const subcategoria = require('./subcategoria');
 
 router.use('/art', articulo);
 router.use('/lib', libro);
 router.use('/prod', producto);
+router.use('/sub', subcategoria);
 
 
 module.exports = router;
---
+++ src/routes/subcategoria.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../database');
+
+
+router.get('/subcategorias', async (req, res)=>{
+
+    try{
+        const subcategorias = await pool.query('SELECT * FROM subcategoria_producto');
+        res.json(subcategorias);
+    }catch(e){
+        res.json(e);
+    }
+});
+
+router.post('/addSubCategoria', async (req, res)=>{
+    const {
+        descripcion,
+        categoria_especifica,
+    } = req.body;
+
+    const newSub ={      
+        descripcion,
+        categoria_especifica,
+    }
+    console.log(newProd);
+    await pool.query('INSERT INTO subcategoria_producto SET ?', [newSub]);
+    res.redirect('subcategorias');
+});
+
+router.get('/deleteSub/:id', async (req, res)=>{
+    const {id} = req.params;
+    await pool.query('DELETE FROM subcategoria_producto WHERE id = ?', [id]);
+    res.redirect('/sub/subcategorias');
+});
+
+router.post('/editSub/:id', async(req, res)=>{
+    const {id} = req.params;
+    const {
+        descripcion,
+        categoria_especifica,
+    } = req.body;
+
+    await pool.query('UPDATE subcategoria_producto SET descripcion = ?, categoria_especifica = ?  WHERE id = ?', [descripcion, categoria_especifica, id]);
+    res.redirect('/sub/subcategorias');
+});
+
+router.get('/findById/:id', async(req, res)=>{
+    const {id} = req.params;
+    let subcategoria = await pool.query('SELECT * FROM subcategoria_producto WHERE id = ?', [id]);
+
+    subcategoria = subcategoria[0];
+    
+    if(subcategoria != null){
+        res.json(subcategoria);
+    }else{
+        msg = 'La subcategoria no existe'
+        res.json(msg);
+    }
+    
+});
+
+
+module.exports = router;
